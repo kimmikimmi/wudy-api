696c9024508f65abad9890056b1512b59ccf17ed
Jaden <bright2013@coupang.com>
2017-06-25 14:37:40 +0900

    Hello World!

--- src/main/java/com/wudy/WudyApiApplication.java
+++ src/main/java/com/wudy/WudyApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WudyApiApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
 		SpringApplication.run(WudyApiApplication.class, args);
 	}
 }
