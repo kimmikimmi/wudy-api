88480c5476764a50a0f15559ca18b512b08e2795
Jaden <bright2013@coupang.com>
2017-06-25 14:27:16 +0900

    first push

---
+++ src/main/java/com/wudy/WudyApiApplication.java
@@ -0,0 +1,12 @@
+package com.wudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WudyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WudyApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wudy/WudyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WudyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
