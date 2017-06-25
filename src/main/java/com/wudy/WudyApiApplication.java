65ebb7660c3cf80345464f6c5b488bf7d7db72b9
mark-lee <mark.lee@snowcorp.com>
2017-06-25 17:10:23 +0900

    plz polling

--- src/main/java/com/wudy/WudyApiApplication.java
+++ src/main/java/com/wudy/WudyApiApplication.java
@@ -2,9 +2,17 @@ package com.wudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WudyApiApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World v1";
+	}
 
 	public static void main(String[] args) {
 
