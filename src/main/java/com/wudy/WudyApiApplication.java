128d1083ef02d05270a658c06f7bd6925ec135a6
Jaden <bright2013@coupang.com>
2017-07-01 16:17:49 +0900

    dsd

--- src/main/java/com/wudy/WudyApiApplication.java
+++ src/main/java/com/wudy/WudyApiApplication.java
@@ -1,6 +1,7 @@
 package com.wudy;
 
 import com.wudy.common.WudyResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +13,17 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class WudyApiApplication {
 
 	@RequestMapping(value = "/article", method = RequestMethod.POST)
 	public WudyResponse article(@RequestParam MultipartFile urlList) throws IOException {
 		System.out.println(urlList);
 		String uuid = UUID.fromString(urlList.getOriginalFilename()).toString();
-		File dest = new File("/wudy-api/"+uuid);
+		File dest = new File("/home/ubuntu/tmp/"+uuid);
 
 		urlList.transferTo(dest);
-
+		log.info("file write success {}", dest.getAbsolutePath());
 		return new WudyResponse();
 	}
 
